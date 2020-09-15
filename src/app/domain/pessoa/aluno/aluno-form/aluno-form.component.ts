1d02fa16648c18b0e65c0be880ed6b1351ec412b
Leandro Hinckel Silveira <leandro.hinckel@jv01.local>
2020-09-15 11:03:45 -0300

    Remover campo "matrícula" do aluno-form

--- src/app/domain/pessoa/aluno/aluno-form/aluno-form.component.ts
+++ src/app/domain/pessoa/aluno/aluno-form/aluno-form.component.ts
@@ -1,15 +1,23 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  Input,
+  EventEmitter,
+  Output,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { Aluno, FormaIngresso } from '../aluno';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { BaseComponent } from 'app/shared/base/base.component';
 import { PoSelectOption } from '@po-ui/ng-components';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-aluno-form',
   templateUrl: './aluno-form.component.html',
   styleUrls: ['./aluno-form.component.css'],
 })
-export class AlunoFormComponent extends BaseComponent {
+export class AlunoFormComponent extends BaseComponent implements OnChanges {
   constructor(private formBuilder: FormBuilder) {
     super();
   }
@@ -17,11 +25,10 @@ export class AlunoFormComponent extends BaseComponent {
   @Input()
   aluno = new Aluno();
 
-  @Output()
-  alunoChange = new EventEmitter<Aluno>();
-
   @Output('submit')
   onSubmit = new EventEmitter<Aluno>();
+
+  formSubscription: Subscription;
 
   form: FormGroup;
 
@@ -40,16 +47,25 @@ export class AlunoFormComponent extends BaseComponent {
     return this.form.valid && !this.form.pending;
   }
 
+  ngOnChanges({ aluno }: SimpleChanges) {
+    if (aluno) this.criarForm();
+  }
+
   ngOnInit() {
+    super.ngOnInit();
+    this.criarForm();
+  }
+
+  criarForm() {
+    this.formSubscription?.unsubscribe();
     this.form = this.aluno.criarForm(this.formBuilder);
-    this.aluno.subscribeFormChanges(
+    this.formSubscription = this.aluno.subscribeFormChanges(
       this.form,
-      this.takeWhileMounted(),
-      (aluno) => this.alunoChange.emit(aluno)
+      this.takeWhileMounted()
     );
   }
 
   submit() {
-    this.onSubmit.emit(this.aluno);
+    this.canSubmit && this.onSubmit.emit(this.aluno);
   }
 }

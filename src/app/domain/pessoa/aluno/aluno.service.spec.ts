82d005e7f3a785748a4a9559d46de4d4a608dda9
Leandro Hinckel Silveira <leandro.hinckel@jv01.local>
2020-09-16 14:01:36 -0300

    Melhorar testes do AlunoService

--- src/app/domain/pessoa/aluno/aluno.service.spec.ts
+++ src/app/domain/pessoa/aluno/aluno.service.spec.ts
@@ -1,17 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 import { AlunoService } from './aluno.service';
 import { injectionToken } from './alunos.mock';
-import { Page, Pageable } from 'app/shared/util/service.util';
-import { Aluno } from './aluno';
+import { Page, Pageable, Sort } from 'app/shared/util/service.util';
+import { Aluno, AlunoSortFields } from './aluno';
 import { take } from 'rxjs/operators';
 import alunosMock, { AlunosMock } from './alunos.mock';
 import { environment } from 'environments/environment';
+import { format as formatCpf } from '@fnando/cpf';
 
 let mockValues: AlunosMock = {
   values: [],
 };
 
-environment.delaySimulado = [100, 300];
+environment.delaySimulado = null;
 
 describe('AlunoService', () => {
   beforeEach(() => {
@@ -108,6 +109,226 @@ describe('AlunoService', () => {
           expect(result.items.length).toEqual(11));
         it('A flag "hasNext" é false', () =>
           expect(result.hasNext).toBeFalse());
+      });
+
+      describe('Informando o valor "TESTE 1" como filtro (para filtrar nome)', () => {
+        let result: Page<Aluno>;
+        beforeEach(async () => {
+          result = await service
+            .buscarAlunosLikeNomeOuEmailOuCpfOuMatricula(
+              new Pageable(),
+              'TESTE 1'
+            )
+            .pipe(take(1))
+            .toPromise();
+        });
+
+        it('O array de items possui 11 elementos', () =>
+          expect(result.items.length).toEqual(11));
+        it('A flag "hasNext" é false', () =>
+          expect(result.hasNext).toBeFalse());
+      });
+
+      describe('Informando o valor "teste1" como filtro (para filtrar email)', () => {
+        let result: Page<Aluno>;
+        beforeEach(async () => {
+          result = await service
+            .buscarAlunosLikeNomeOuEmailOuCpfOuMatricula(
+              new Pageable(),
+              'teste1'
+            )
+            .pipe(take(1))
+            .toPromise();
+        });
+
+        it('O array de items possui 11 elementos', () =>
+          expect(result.items.length).toEqual(11));
+        it('A flag "hasNext" é false', () =>
+          expect(result.hasNext).toBeFalse());
+      });
+
+      describe('Informando o valor "TESTE1" como filtro (para filtrar email)', () => {
+        let result: Page<Aluno>;
+        beforeEach(async () => {
+          result = await service
+            .buscarAlunosLikeNomeOuEmailOuCpfOuMatricula(
+              new Pageable(),
+              'TESTE1'
+            )
+            .pipe(take(1))
+            .toPromise();
+        });
+
+        it('O array de items possui 11 elementos', () =>
+          expect(result.items.length).toEqual(11));
+        it('A flag "hasNext" é false', () =>
+          expect(result.hasNext).toBeFalse());
+      });
+
+      describe('Informando o valor "ena" como filtro (para filtrar formaIngresso)', () => {
+        let result: Page<Aluno>;
+        let expectedLength: number;
+        let expectedHasNext: boolean;
+        beforeEach(async () => {
+          result = await service
+            .buscarAlunosLikeNomeOuEmailOuCpfOuMatricula(new Pageable(), 'ena')
+            .pipe(take(1))
+            .toPromise();
+
+          expectedLength = mockValues.values.filter(
+            (aluno) => aluno.formaIngresso === 'ENADE'
+          ).length;
+          expectedHasNext = expectedLength > 20;
+          expectedLength = expectedHasNext ? 20 : expectedLength;
+        });
+
+        it('O array de items possui 11 elementos', () =>
+          expect(result.items.length).toEqual(expectedLength));
+        it('A flag "hasNext" é false', () =>
+          expect(result.hasNext).toEqual(expectedHasNext));
+      });
+
+      describe('Informando o valor "ves" como filtro (para filtrar formaIngresso)', () => {
+        let result: Page<Aluno>;
+        let expectedLength: number;
+        let expectedHasNext: boolean;
+        beforeEach(async () => {
+          result = await service
+            .buscarAlunosLikeNomeOuEmailOuCpfOuMatricula(new Pageable(), 'ves')
+            .pipe(take(1))
+            .toPromise();
+
+          expectedLength = mockValues.values.filter(
+            (aluno) => aluno.formaIngresso === 'Vestibular'
+          ).length;
+          expectedHasNext = expectedLength > 20;
+          expectedLength = expectedHasNext ? 20 : expectedLength;
+        });
+
+        it('O array de items possui 11 elementos', () =>
+          expect(result.items.length).toEqual(expectedLength));
+        it('A flag "hasNext" é false', () =>
+          expect(result.hasNext).toEqual(expectedHasNext));
+      });
+
+      describe('Informando o cpf do primeiro elemento como valor do filtro (para filtrar CPF, sem formatação)', () => {
+        let result: Page<Aluno>;
+        beforeEach(async () => {
+          result = await service
+            .buscarAlunosLikeNomeOuEmailOuCpfOuMatricula(
+              new Pageable(),
+              mockValues.values[0].cpf
+            )
+            .pipe(take(1))
+            .toPromise();
+        });
+
+        it('O array de items possui 1 elementos', () =>
+          expect(result.items.length).toEqual(1));
+        it('A flag "hasNext" é false', () =>
+          expect(result.hasNext).toBeFalse());
+      });
+
+      describe('Informando o cpf do primeiro elemento como valor do filtro (para filtrar CPF, com formatação)', () => {
+        let result: Page<Aluno>;
+        beforeEach(async () => {
+          result = await service
+            .buscarAlunosLikeNomeOuEmailOuCpfOuMatricula(
+              new Pageable(),
+              formatCpf(mockValues.values[0].cpf)
+            )
+            .pipe(take(1))
+            .toPromise();
+        });
+
+        it('O array de items possui 1 elementos', () =>
+          expect(result.items.length).toEqual(1));
+        it('A flag "hasNext" é false', () =>
+          expect(result.hasNext).toBeFalse());
+      });
+
+      describe('Informando o sort pelo nome e asc', () => {
+        let result: Page<Aluno>;
+        beforeEach(async () => {
+          result = await service
+            .buscarAlunosLikeNomeOuEmailOuCpfOuMatricula(
+              new Pageable(),
+              '',
+              new Sort<AlunoSortFields>('nome', 'asc')
+            )
+            .pipe(take(1))
+            .toPromise();
+          console.log(JSON.stringify(result.items));
+        });
+
+        it('O array de items possui 20 elementos', () =>
+          expect(result.items.length).toEqual(20));
+        it('A flag "hasNext" é true', () => expect(result.hasNext).toBeTrue());
+        it('O array de items está ordenado com ordem crescente', () =>
+          expect(result.items.map((aluno) => aluno.nome)).toEqual([
+            'Teste 1',
+            'Teste 10',
+            'Teste 11',
+            'Teste 12',
+            'Teste 13',
+            'Teste 14',
+            'Teste 15',
+            'Teste 16',
+            'Teste 17',
+            'Teste 18',
+            'Teste 19',
+            'Teste 2',
+            'Teste 20',
+            'Teste 21',
+            'Teste 22',
+            'Teste 23',
+            'Teste 24',
+            'Teste 25',
+            'Teste 26',
+            'Teste 27',
+          ]));
+      });
+
+      describe('Informando o sort pelo nome e desc', () => {
+        let result: Page<Aluno>;
+        beforeEach(async () => {
+          result = await service
+            .buscarAlunosLikeNomeOuEmailOuCpfOuMatricula(
+              new Pageable(),
+              '',
+              new Sort<AlunoSortFields>('nome', 'desc')
+            )
+            .pipe(take(1))
+            .toPromise();
+          console.log(JSON.stringify(result.items));
+        });
+
+        it('O array de items possui 20 elementos', () =>
+          expect(result.items.length).toEqual(20));
+        it('A flag "hasNext" é true', () => expect(result.hasNext).toBeTrue());
+        it('O array de items está ordenado com ordem decrescente', () =>
+          expect(result.items.map((aluno) => aluno.nome)).toEqual([
+            'Teste 9',
+            'Teste 8',
+            'Teste 7',
+            'Teste 6',
+            'Teste 50',
+            'Teste 5',
+            'Teste 49',
+            'Teste 48',
+            'Teste 47',
+            'Teste 46',
+            'Teste 45',
+            'Teste 44',
+            'Teste 43',
+            'Teste 42',
+            'Teste 41',
+            'Teste 40',
+            'Teste 4',
+            'Teste 39',
+            'Teste 38',
+            'Teste 37',
+          ]));
       });
     });
   });

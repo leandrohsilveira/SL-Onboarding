b87483bb51b4acb01ca996b369bcd3ad76af905b
Leandro Hinckel Silveira <leandro.hinckel@jv01.local>
2020-09-18 10:49:02 -0300

    Add rota para cadastro de alunos

--- src/app/domain/pessoa/aluno/aluno.routing.ts
+++ src/app/domain/pessoa/aluno/aluno.routing.ts
@@ -3,10 +3,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlunoListRouteModule } from './routes/aluno-list-route/aluno-list-route.module';
 import { AlunoListRouteComponent } from './routes/aluno-list-route/aluno-list-route.component';
+import { AlunoFormRouteModule } from './routes/aluno-form-route/aluno-form-route.module';
+import { AlunoFormRouteComponent } from './routes/aluno-form-route/aluno-form-route.component';
 
-const routes: Routes = [{ path: '', component: AlunoListRouteComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: AlunoListRouteComponent,
+    children: [
+      {
+        path: 'new',
+        pathMatch: 'full',
+        component: AlunoFormRouteComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
-  imports: [CommonModule, AlunoListRouteModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    AlunoListRouteModule,
+    AlunoFormRouteModule,
+    RouterModule.forChild(routes),
+  ],
 })
 export class AlunoRoutingModule {}

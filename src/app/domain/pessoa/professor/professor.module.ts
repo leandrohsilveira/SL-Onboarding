fc3210cb0e74b0008cd97b3f8d2cb2992d00b4a1
Leandro Hinckel Silveira <leandro.hinckel@jv01.local>
2020-09-22 17:00:13 -0300

    Remover ProfessorComponent

--- src/app/domain/pessoa/professor/professor.module.ts
+++ src/app/domain/pessoa/professor/professor.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProfessorComponent } from './professor.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppBackendModule } from 'backend/app-backend.module';
 import { ProfessorService } from './professor.service';
@@ -8,6 +7,5 @@ import { ProfessorService } from './professor.service';
 @NgModule({
   imports: [CommonModule, HttpClientModule, AppBackendModule.forFeature()],
   providers: [ProfessorService],
-  declarations: [ProfessorComponent]
 })
 export class ProfessorModule {}

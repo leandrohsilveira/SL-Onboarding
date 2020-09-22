fee01d2c933c9b63ecb2ff8bf6b809cdcae7562f
Leandro Hinckel Silveira <leandro.hinckel@jv01.local>
2020-09-22 16:47:54 -0300

    Criar backend mock de professores

---
+++ src/backend/domain/pessoa/professor/professor.data.ts
@@ -0,0 +1,31 @@
+import { dataService } from 'web-backend-api';
+import professores from './professor.mock.json';
+import { endpoints } from 'app/shared/endpoints';
+import { createSearchRequestInterceptor } from 'backend/interceptors';
+import { searchString } from 'app/shared/util/service.util';
+
+export const collectionName = 'professor';
+
+export function setup(data = professores) {
+  dataService(collectionName, (db) => {
+    db.addReplaceUrl(collectionName, endpoints.core.v1.professores.path);
+    db.addReplaceUrl(collectionName, endpoints.query.v1.professores.path);
+
+    db.addRequestInterceptor(
+      createSearchRequestInterceptor(
+        collectionName,
+        endpoints.query.v1.alunos.path,
+        (searchTerm) => (item) =>
+          searchTerm === '' ||
+          searchString(item.nome, searchTerm) ||
+          searchString(item.cpf, searchTerm.replace(/(\.|\-)/g, '')) ||
+          searchString(item.email, searchTerm) ||
+          searchString(item.titulacao, searchTerm)
+      )
+    );
+
+    data.forEach((item) => {
+      db.storeData(collectionName, item);
+    });
+  });
+}

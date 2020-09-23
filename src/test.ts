4a90ffa95a1bf6802302b31e984f5e678d255aef
Leandro Hinckel Silveira <leandro.hinckel@jv01.local>
2020-09-23 17:58:29 -0300

    Configurar web-backend-api nos testes

--- src/test.ts
+++ src/test.ts
@@ -4,22 +4,41 @@ import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
+  platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
+import { setupBackend } from 'web-backend-api';
+import { backendConfig } from 'backend/config';
 
 declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
+  context(
+    path: string,
+    deep?: boolean,
+    filter?: RegExp
+  ): {
     keys(): string[];
     <T>(id: string): T;
   };
 };
 
-// First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
-);
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().forEach(context);
+const backend = require.context('./backend/', true, /\.data\.ts$/);
+backend.keys().forEach((mod: any) => {
+  const { setup } = backend(mod);
+  if (setup && typeof setup === 'function') {
+    setup([]);
+  } else {
+    console.warn(
+      `Módulo de backend "${mod}" não exporta uma função "setup", a coleção não foi inicializada!`
+    );
+  }
+});
+
+setupBackend(backendConfig, { dbtype: 'memory' }).then(() => {
+  getTestBed().initTestEnvironment(
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting()
+  );
+  // Then we find all the tests.
+  const context = require.context('./', true, /\.spec\.ts$/);
+  // And load the modules.
+  context.keys().forEach(context);
+});

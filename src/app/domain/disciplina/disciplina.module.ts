0f2a1ad770d2b91a4dc84329e43948cb3e370816
Leandro Hinckel Silveira <leandro.hinckel@jv01.local>
2020-09-23 14:16:30 -0300

    Remover componente disciplina

--- src/app/domain/disciplina/disciplina.module.ts
+++ src/app/domain/disciplina/disciplina.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DisciplinaComponent } from './disciplina.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [DisciplinaComponent]
+  imports: [CommonModule],
+  declarations: [],
 })
-export class DisciplinaModule { }
+export class DisciplinaModule {}

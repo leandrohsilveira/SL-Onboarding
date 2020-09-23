8103e1d18fcb301d768ae7febbdfa9a86efd28e1
Leandro Hinckel Silveira <leandro.hinckel@jv01.local>
2020-09-23 11:02:52 -0300

    Add rotas de cadastro e atualização de professores

--- src/app/domain/pessoa/professor/professor.routing.ts
+++ src/app/domain/pessoa/professor/professor.routing.ts
@@ -3,11 +3,35 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ProfessorListRouteModule } from './routes/professor-list-route/professor-list-route.module';
 import { ProfessorListRouteComponent } from './routes/professor-list-route/professor-list-route.component';
+import { ProfessorFormRouteModule } from './routes/professor-form-route/professor-form-route.module';
+import { ProfessorFormRouteComponent } from './routes/professor-form-route/professor-form-route.component';
+import { RouterUtil } from 'app/shared/util/router.util';
 
 const routes: Routes = [
   {
     path: '',
     component: ProfessorListRouteComponent,
+    children: [
+      {
+        path: 'new',
+        component: ProfessorFormRouteComponent,
+        data: {
+          urlRetorno: RouterUtil.urlFromRootToParent,
+          mensagemSucesso: () =>
+            $localize`:Mensagem de sucesso ao cadastrar um novo professor:Professor cadastrado com sucesso`,
+        },
+      },
+      {
+        path: ':id/edit',
+        component: ProfessorFormRouteComponent,
+        data: {
+          loadFromParam: 'id',
+          urlRetorno: RouterUtil.urlFromRootToParent,
+          mensagemSucesso: () =>
+            $localize`:Mensagem de sucesso ao atualizar os dados de um professor existente:Professor atualizado com sucesso`,
+        },
+      },
+    ],
   },
 ];
 
@@ -15,6 +39,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
     ProfessorListRouteModule,
+    ProfessorFormRouteModule,
     RouterModule.forChild(routes),
   ],
 })

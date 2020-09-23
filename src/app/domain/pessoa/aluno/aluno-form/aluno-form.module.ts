ef46232e919518c11400499bbe8561f8104494df
Leandro Hinckel Silveira <leandro.hinckel@jv01.local>
2020-09-23 09:13:36 -0300

    Mover componente field-messages

--- src/app/domain/pessoa/aluno/aluno-form/aluno-form.module.ts
+++ src/app/domain/pessoa/aluno/aluno-form/aluno-form.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { AlunoFormComponent } from './aluno-form.component';
 import { PoFieldModule, PoButtonModule } from '@po-ui/ng-components';
 import { ReactiveFormsModule } from '@angular/forms';
-import { FieldMessagesModule } from 'app/shared/field-messages/field-messages.module';
+import { FieldMessagesModule } from 'app/shared/components/field-messages/field-messages.module';
 
 @NgModule({
   imports: [
--- src/app/domain/pessoa/professor/professor-form/professor-form.module.ts
+++ src/app/domain/pessoa/professor/professor-form/professor-form.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfessorFormComponent } from './professor-form.component';
 import { PoFieldModule } from '@po-ui/ng-components';
-import { FieldMessagesModule } from 'app/shared/field-messages/field-messages.module';
+import { FieldMessagesModule } from 'app/shared/components/field-messages/field-messages.module';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({

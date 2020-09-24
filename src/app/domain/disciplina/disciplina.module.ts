2f1aa28ab2f7df6bde756022bcad47f623e7dfbc
Leandro Hinckel Silveira <leandro.hinckel@jv01.local>
2020-09-24 14:23:40 -0300

    Add dependencias do DisciplinaModule

--- src/app/domain/disciplina/disciplina.module.ts
+++ src/app/domain/disciplina/disciplina.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { AppBackendModule } from 'backend/app-backend.module';
+import { DisciplinaService } from './disciplina.service';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [],
+  imports: [CommonModule, HttpClientModule, AppBackendModule.forRoot()],
+  providers: [DisciplinaService],
 })
 export class DisciplinaModule {}

858ba570b63bfc26bd8d41a7ae1671b996b7950b
Leandro Hinckel Silveira <leandro.hinckel@jv01.local>
2020-09-25 16:36:12 -0300

    Add rotas de formulario de disicplina

--- src/app/domain/disciplina/disciplina.routing.ts
+++ src/app/domain/disciplina/disciplina.routing.ts
@@ -3,17 +3,59 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DisciplinaListRouteModule } from './routes/disciplina-list-route/disciplina-list-route.module';
 import { DisciplinaListRouteComponent } from './routes/disciplina-list-route/disciplina-list-route.component';
+import { DisciplinaFormRouteComponent } from './routes/disciplina-form-route/disciplina-form-route.component';
+import { RouterUtil } from 'app/shared/util/router.util';
+import { DisciplinaFormRouteModule } from './routes/disciplina-form-route/disciplina-form-route.module';
+import { ProfessorFormRouteModule } from '../pessoa/professor/routes/professor-form-route/professor-form-route.module';
+import { ProfessorFormRouteComponent } from '../pessoa/professor/routes/professor-form-route/professor-form-route.component';
+
+const formChildren: Routes = [
+  {
+    path: 'professor/new',
+    component: ProfessorFormRouteComponent,
+    data: {
+      urlRetorno: RouterUtil.urlFromRootToParent,
+      mensagemSucesso: () =>
+        $localize`:Mensagem de sucesso ao cadastrar um novo professor para uma disciplina:Professor cadastrado com sucesso`,
+    },
+  },
+];
 
 const routes: Routes = [
   {
     path: '',
     component: DisciplinaListRouteComponent,
+    children: [
+      {
+        path: 'new',
+        component: DisciplinaFormRouteComponent,
+        data: {
+          urlRetorno: RouterUtil.urlFromRootToParent,
+          mensagemSucesso: () =>
+            $localize`:Mensagem de sucesso ao cadastrar uma nova disciplina:Disciplina cadastrada com sucesso`,
+        },
+        children: formChildren,
+      },
+      {
+        path: ':id/edit',
+        component: DisciplinaFormRouteComponent,
+        data: {
+          loadFromParam: 'id',
+          urlRetorno: RouterUtil.urlFromRootToParent,
+          mensagemSucesso: () =>
+            $localize`:Mensagem de sucesso ao atualizar um disciplina existente:Disciplina atualizada com sucesso`,
+        },
+        children: formChildren,
+      },
+    ],
   },
 ];
 
 @NgModule({
   imports: [
     CommonModule,
+    ProfessorFormRouteModule,
+    DisciplinaFormRouteModule,
     DisciplinaListRouteModule,
     RouterModule.forChild(routes),
   ],
